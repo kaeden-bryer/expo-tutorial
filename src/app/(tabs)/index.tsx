666ab4e9a7ec2c76761e6dcce31ad37344ecd96b
Kaeden Bryer <kaedenbryer@gmail.com>
2026-05-31 10:17:34 -0400

    chapter 3: creating a page

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,13 +1,19 @@
 import { Text, View, StyleSheet } from "react-native";
-import { Link } from "expo-router";
+import ImageViewer from '@/components/ImageViewer'
+import Button from "@/components/Button";
+
+const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Home screen</Text>
-      <Link href="/about" style={styles.button}>
-        Go to About Screen
-      </Link>
+      <View style={styles.imageContainer}>
+        <ImageViewer imgSource={PlaceholderImage} />
+      </View>
+      <View style={styles.footerContainer}>
+        <Button theme="primary" label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
     </View>
   );
 }
@@ -17,14 +23,17 @@ const styles = StyleSheet.create({
     backgroundColor: "#25292e",
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
   },
-  text: {
-    color: 'white'
+  imageContainer: {
+    flex: 1,
   },
-  button: {
-    fontSize: 20,
-    textDecorationLine: 'underline',
-    color: "white"
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18
+  },
+  footerContainer: {
+    flex: 1/3,
+    alignItems: 'center'
   }
 });
